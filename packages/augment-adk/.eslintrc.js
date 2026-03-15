c46a387ba8b94303774b5a7a7d51d72ec048af05
augment-adk <adk@augment.dev>
2026-03-14 23:44:33 -0400

    fix: add ESLint config for standalone CI
    ESLint requires a config file when run in CI where no parent
    workspace config is available.

---
+++ packages/augment-adk/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-console': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.test.ts'],
+};
